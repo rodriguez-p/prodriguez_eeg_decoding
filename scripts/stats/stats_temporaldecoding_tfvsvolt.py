9b637a8e9701347d20810f0c4cc1f8a0535e44c1
rodriguez-p <106660817+rodriguez-p@users.noreply.github.com>
2023-11-14 16:46:39 +0100

    Add files via upload

--- scripts/stats/stats_temporaldecoding_tfvsvolt.py
+++ scripts/stats/stats_temporaldecoding_tfvsvolt.py
@@ -1,22 +1,23 @@
 import numpy as np
 from scipy.stats import sem
-from plotting import plot_stats_tfvsvoltage
-from stats import wilcoxon_fdr_tempdecod
-from utils import load_scores
+from plotting_functions import plot_stats_tfvsvoltage
+from stats_functions import wilcoxon_fdr_tempdecod
+from utils_functions import load_scores
+import os.path
+import xlsxwriter
 
 #%%
-FIGURES_DIR = 'C:/Users/Profa. Ana Chica/Desktop/prodriguez_eeg_decoding/figures'
-PKL_PATH = 'C:/Users/Profa. Ana Chica/Desktop/prodriguez_eeg_decoding/data'
-DECOD_TFR = 'C:/Users/Profa. Ana Chica/Desktop/prodriguez_eeg_decoding/data/decoding_time-frequency'
-DECOD_TIME = 'C:/Users/Profa. Ana Chica/Desktop/prodriguez_eeg_decoding/data/decoding_time'
+FIGURES_DIR = r'C:/Users/Profa. Ana Chica/Desktop/prodriguez_eeg_decoding/figures'
+DATA_PATH = r'F:/EEG_Decoding_Offline/prodriguez_eeg_decoding/data'
+DECOD_TFR = os.path.join(DATA_PATH, 'decoding_time-frequency')
+DECOD_TIME = os.path.join(DATA_PATH, 'decoding_time')
 
 #%%
-times= np.load("{}/{}".format(PKL_PATH, 'times.npy'), allow_pickle=True) 
+times= np.load("{}/{}".format(DATA_PATH, 'times.npy'), allow_pickle=True) 
 
 #%% target presence 
-
-presence_volt = load_scores(DECOD_TIME, 'temporal_decoding_presence_lsvm_experimental')
-presence_tf = load_scores(DECOD_TFR, 'temporal_decoding_time-frequency_presence_lsvm')
+presence_volt = load_scores(DECOD_TIME, 'temporal_decoding_presence_lsvm_experimental', average = False)
+presence_tf = load_scores(DECOD_TFR, 'temporal_decoding_time-frequency_presence_lsvm', average = False)
 
 presence_tf_se = np.mean(sem(presence_tf), axis = 0)
 presence_tf_avg = np.mean(presence_tf, axis = 0)
@@ -29,7 +30,9 @@ presence_volt_avg = np.mean(presence_volt, axis = 0)
 x = presence_tf
 y = presence_volt
 
-reject, p_corrected, presence_sign_points = wilcoxon_fdr_tempdecod(x, y, times)
+stats_file = 'stats_temporal_decoding_tfvsvolt_presence'
+
+reject, p_corrected, presence_sign_points = wilcoxon_fdr_tempdecod(x, y, times, stats_file)
 
 # plot data with dots for significant comparisons
 
@@ -41,9 +44,8 @@ plot_stats_tfvsvoltage(presence_tf_avg, presence_volt_avg, presence_tf_se, prese
                         times, presence_sign_points, 0.46, title, save_figure)
 
 #%% subject awareness 
-
-awareness_volt = load_scores(DECOD_TIME, 'temporal_decoding_awareness_lsvm_experimental')
-awareness_tf = load_scores(DECOD_TFR, 'temporal_decoding_time-frequency_awareness_lsvm')
+awareness_volt = load_scores(DECOD_TIME, 'temporal_decoding_awareness_lsvm_experimental', average = False)
+awareness_tf = load_scores(DECOD_TFR, 'temporal_decoding_time-frequency_awareness_lsvm', average = False)
 
 awareness_tf_se = np.mean(sem(awareness_tf), axis = 0)
 awareness_tf_avg = np.mean(awareness_tf, axis = 0)
@@ -56,10 +58,11 @@ awareness_volt_avg = np.mean(awareness_volt, axis = 0)
 x = awareness_tf
 y = awareness_volt
 
-reject, p_corrected, awareness_sign_points = wilcoxon_fdr_tempdecod(x, y, times)
+stats_file = 'stats_temporal_decoding_tfvsvolt_awareness'
+
+reject, p_corrected, awareness_sign_points = wilcoxon_fdr_tempdecod(x, y, times, stats_file)
 
 # plot data with dots for significant comparisons
-
 title='Subject awareness - TF power vs voltage'
 save_figure = "{}/{}".format(FIGURES_DIR, title)
 
@@ -68,9 +71,8 @@ plot_stats_tfvsvoltage(awareness_tf_avg, awareness_volt_avg, awareness_tf_se, aw
                         times, awareness_sign_points, 0.46, title, save_figure)
 
 #%% gabor tilt 
-
-tilt_volt = load_scores(DECOD_TIME, 'temporal_decoding_tilt_lsvm_experimental')
-tilt_tf = load_scores(DECOD_TFR, 'temporal_decoding_time-frequency_tilt_lsvm')
+tilt_volt = load_scores(DECOD_TIME, 'temporal_decoding_tilt_lsvm_experimental', average = False)
+tilt_tf = load_scores(DECOD_TFR, 'temporal_decoding_time-frequency_tilt_lsvm', average = False)
 
 tilt_tf_se = np.mean(sem(tilt_tf), axis = 0)
 tilt_tf_avg = np.mean(tilt_tf, axis = 0)
@@ -83,10 +85,11 @@ tilt_volt_avg = np.mean(tilt_volt, axis = 0)
 x = tilt_tf
 y = tilt_volt
 
-reject, p_corrected, tilt_sign_points = wilcoxon_fdr_tempdecod(x, y, times)
+stats_file = 'stats_temporal_decoding_tfvsvolt_tilt'
+
+reject, p_corrected, tilt_sign_points = wilcoxon_fdr_tempdecod(x, y, times, stats_file)
 
 # plot data with dots for significant comparisons
-
 title='Gabor tilt - TF power vs voltage'
 save_figure = "{}/{}".format(FIGURES_DIR, title)
 
