bc1cc0afdb3c6b882bd41e3536a41d90f6c04727
rodriguez-p <106660817+rodriguez-p@users.noreply.github.com>
2023-11-14 16:45:34 +0100

    Add files via upload

--- scripts/stats/stats_temporaldecoding_tfvschance.py
+++ scripts/stats/stats_temporaldecoding_tfvschance.py
@@ -3,14 +3,15 @@ import numpy as np
 from scipy.stats import sem
 from stats_functions import wilcoxon_fdr_tempdecod
 from utils_functions import load_scores
+import seaborn as sns
 
 #%%
 FIGURES_DIR = 'C:/Users/Profa. Ana Chica/Desktop/prodriguez_eeg_decoding/figures'
-PKL_PATH = 'C:/Users/Profa. Ana Chica/Desktop/prodriguez_eeg_decoding/data'
-DECOD_TFR = 'C:/Users/Profa. Ana Chica/Desktop/prodriguez_eeg_decoding/data/decoding_time-frequency'
+DATA_PATH = 'F:/EEG_Decoding_Offline/prodriguez_eeg_decoding/data'
+DECOD_TFR = 'F:/EEG_Decoding_Offline/prodriguez_eeg_decoding/data/decoding_time-frequency'
 
 #%%
-times= np.load("{}/{}".format(PKL_PATH, 'times.npy'), allow_pickle=True) 
+times= np.load("{}/{}".format(DATA_PATH, 'times.npy'), allow_pickle=True) 
 
 #%% target presence 
 
@@ -23,7 +24,9 @@ presence_tf_avg = np.mean(presence_tf, axis = 0)
 x = presence_tf
 y = np.full((30, 1024), 0.5)
 
-reject, p_corrected, presence_sign_points = wilcoxon_fdr_tempdecod(x, y, times)
+stats_file = 'stats_temporal_decoding_tfvschance_presence'
+
+reject, p_corrected, presence_sign_points = wilcoxon_fdr_tempdecod(x, y, times, stats_file)
 
 #%% subject awareness
 
@@ -36,8 +39,9 @@ awareness_tf_avg = np.mean(awareness_tf, axis = 0)
 x = awareness_tf
 y = np.full((30, 1024), 0.5)
 
-# Wilcoxon signed-rank test with FDR correction
-reject, p_corrected, awareness_sign_points = wilcoxon_fdr_tempdecod(x, y, times)
+stats_file = 'stats_temporal_decoding_tfvschance_awareness'
+
+reject, p_corrected, awareness_sign_points = wilcoxon_fdr_tempdecod(x, y, times, stats_file)
 
 #%% gabor tilt
 
@@ -50,12 +54,16 @@ tilt_tf_avg = np.mean(tilt_tf, axis = 0)
 x = tilt_tf
 y = np.full((30, 1024), 0.5)
 
-reject, p_corrected, tilt_sign_points = wilcoxon_fdr_tempdecod(x, y, times)
+stats_file = 'stats_temporal_decoding_tfvschance_tilt'
+
+reject, p_corrected, tilt_sign_points = wilcoxon_fdr_tempdecod(x, y, times, stats_file)
 
 #%% plot data with dots for significant comparisons
 
 title='Plot stats TF power'
 save_figure = "{}/{}".format(FIGURES_DIR, title)
+custom_params = {"axes.spines.right": False, "axes.spines.top": False}
+sns.set_theme(style="ticks", rc=custom_params)
 
 data_to_plot = {
     'Target presence': (presence_tf_avg, presence_tf_se, 'navy', 'lavender'),
@@ -70,9 +78,10 @@ for label, (avg_data, se_data, color, color_fill) in data_to_plot.items():
     ax.fill_between(times, (avg_data + se_data), (avg_data - se_data), color=color_fill, alpha=0.5)
 
 ax.axhline(0.5, color='k', linestyle='--', label='Chance level', linewidth=0.5)
+ax.tick_params(axis=u'both', which=u'both',length=0)
 ax.set_xlabel('Times')
 ax.set_ylabel('AUC')  # Area Under the Curve
-ax.legend()
+ax.legend(loc = 'upper left')
 ax.axvline(0.0, color='k', linestyle='-', linewidth=0.5)
 
 for i in presence_sign_points:
@@ -87,8 +96,7 @@ for k in tilt_sign_points:
     plot_points = [0.47]*1024
     ax.scatter(times[k], plot_points[k], color='coral', alpha=0.2, s = 0.1)
 
-ax.spines[['right', 'top']].set_visible(False)
 ax.set_title(title)
-fig.set_size_inches(15, 10)
+# fig.set_size_inches(10, 5)
 plt.savefig(save_figure)
-# plt.close()
+plt.close()
