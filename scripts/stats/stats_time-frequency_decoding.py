53e4a9845abd9f0665c3f3a04869e64dd6796370
rodriguez-p <106660817+rodriguez-p@users.noreply.github.com>
2023-11-14 17:35:31 +0100

    Add files via upload

--- scripts/stats/stats_time-frequency_decoding.py
+++ scripts/stats/stats_time-frequency_decoding.py
@@ -1,20 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.stats import sem
 from stats_functions import wilcoxon_fdr_tfdecod
 from utils_functions import load_scores_tf
-from mne.time_frequency import AverageTFR
-from mne import Epochs, create_info
+from plotting_functions import plot_stats_timefrequency_decoding
 
 #%%
-FIGURES_DIR = 'C:/Users/Profa. Ana Chica/Desktop/prodriguez_eeg_decoding/figures'
-PKL_PATH = 'C:/Users/Profa. Ana Chica/Desktop/prodriguez_eeg_decoding/data'
-DECOD_TFR = 'C:/Users/Profa. Ana Chica/Desktop/prodriguez_eeg_decoding/data/decoding_time-frequency'
+FIGURES_DIR = 'F:/EEG_Decoding_Offline/prodriguez_eeg_decoding/figures'
+DATA_PATH = 'F:/EEG_Decoding_Offline/prodriguez_eeg_decoding/data'
+DECOD_TFR = f'{DATA_PATH}/decoding_time-frequency'
 
-#%% specify freqs
-sfreq=256
-freqs = np.logspace(*np.log10([1, 40]), num=25)
-times= np.load("{}/{}".format(PKL_PATH, 'times.npy'), allow_pickle=True) 
+#%% 
+times= np.load("{}/{}".format(DATA_PATH, 'times.npy'), allow_pickle=True) 
 
 #%% target presence 
 presence_tf = load_scores_tf(DECOD_TFR, 'decoding_time-frequency_presence_lsvm', average = False) 
@@ -22,16 +18,18 @@ presence_tf = load_scores_tf(DECOD_TFR, 'decoding_time-frequency_presence_lsvm',
 presence_tf_avg = np.mean(presence_tf, axis = 0)
 
 # Wilcoxon signed-rank test with FDR correction
-x = presence_tf
-reject, p_corrected, presence_sign_points = wilcoxon_fdr_tfdecod(x)
+
+stats_file = 'stats_time-frequency_decoding_presence'
+
+reject, p_corrected, presence_sign_points = wilcoxon_fdr_tfdecod(presence_tf, times, stats_file)
 
 # plot results with contour for significant time-frequency bins
 title = "Decoding target presence on time-frequency data"
-av_tfr = AverageTFR(create_info(['freq'], sfreq), presence_tf_avg[np.newaxis, :], freqs = freqs[0:], nave=0, times=times)
-av_tfr.plot('all', title=title, cmap='Blues', vmin = 0.5, vmax = np.abs(presence_tf_avg).max(), mask = presence_sign_points, mask_style = 'contour', yscale='linear')
-plt.gcf().set_size_inches(15, 10)
-plt.savefig(f"{FIGURES_DIR}/{title}")
-plt.close()
+color = 'Blues'
+save_figure = f"{FIGURES_DIR}/{title}"
+
+plot_stats_timefrequency_decoding(presence_tf_avg, presence_sign_points, color, times, title,
+                                      save_figure)                                   
  
 #%% subject awareness
 awareness_tf = load_scores_tf(DECOD_TFR, 'decoding_time-frequency_awareness_lsvm', average = False) 
@@ -39,16 +37,18 @@ awareness_tf = load_scores_tf(DECOD_TFR, 'decoding_time-frequency_awareness_lsvm
 awareness_tf_avg = np.mean(awareness_tf, axis = 0)
 
 # Wilcoxon signed-rank test with FDR correction
-x = awareness_tf
-reject, p_corrected, awareness_sign_points = wilcoxon_fdr_tfdecod(x)
+
+stats_file = 'stats_time-frequency_decoding_awareness'
+
+reject, p_corrected, awareness_sign_points = wilcoxon_fdr_tfdecod(awareness_tf, times, stats_file)
 
 # plot results with contour for significant time-frequency bins
 title = "Decoding subject awareness on time-frequency data"
-av_tfr = AverageTFR(create_info(['freq'], sfreq), awareness_tf_avg[np.newaxis, :], freqs = freqs[0:], nave=0, times=times)
-av_tfr.plot('all', title=title, cmap='Reds', vmin = 0.5, vmax = np.abs(awareness_tf_avg).max(), mask = awareness_sign_points, mask_style = 'contour', yscale='linear')
-plt.gcf().set_size_inches(15, 10)
-plt.savefig(f"{FIGURES_DIR}/{title}")
-plt.close()
+color = 'Reds'
+save_figure = f"{FIGURES_DIR}/{title}"
+
+plot_stats_timefrequency_decoding(awareness_tf_avg, awareness_sign_points, color, times, title,
+                                      save_figure)         
 
 #%% gabor tilt
 tilt_tf = load_scores_tf(DECOD_TFR, 'decoding_time-frequency_tilt_lsvm', average = False) 
@@ -56,13 +56,52 @@ tilt_tf = load_scores_tf(DECOD_TFR, 'decoding_time-frequency_tilt_lsvm', average
 tilt_tf_avg = np.mean(tilt_tf, axis = 0)
 
 # Wilcoxon signed-rank test with FDR correction
-x = tilt_tf
-reject, p_corrected, tilt_sign_points = wilcoxon_fdr_tfdecod(x)
+
+stats_file = 'stats_time-frequency_decoding_tilt'
+reject, p_corrected, tilt_sign_points = wilcoxon_fdr_tfdecod(tilt_tf, times, stats_file)
 
 # plot results with contour for significant time-frequency bins
 title = "Decoding Gabor tilt on time-frequency data"
-av_tfr = AverageTFR(create_info(['freq'], sfreq), tilt_tf_avg[np.newaxis, :], freqs = freqs[0:], nave=0, times=times)
-av_tfr.plot('all', title=title, cmap='Greens', vmin = 0.5, vmax = np.abs(tilt_tf_avg).max(), mask = tilt_sign_points, mask_style = 'contour', yscale='linear')
+color = 'Greens'
+save_figure = f"{FIGURES_DIR}/{title}"
+
+plot_stats_timefrequency_decoding(tilt_tf_avg, tilt_sign_points, color, times, title,
+                                      save_figure)  
+
+#%% Target presence seen vs unseen
+
+presence_seen = load_scores_tf(DECOD_TFR, 'decoding_time-frequency_presence-seen', average = False) 
+presence_seen_avg = np.mean(presence_seen, axis = 0)
+
+# # Wilcoxon signed-rank test with FDR correction
+x = presence_seen
+reject, p_corrected, sign_points = wilcoxon_fdr_tfdecod(x)
+
+# plot results with contour for significant time-frequency bins
+title = "Decoding Gabor tilt on time-frequency data"
+av_tfr = AverageTFR(create_info(['freq'], sfreq), presence_seen_avg[np.newaxis, :], freqs = freqs[0:], nave=0, times=times)
+av_tfr.plot('all', title=title, cmap='Blues', vmin = 0.5, vmax = np.abs(presence_seen_avg).max(), 
+            mask = sign_points, mask_style = 'contour', yscale='linear'
+            )
 plt.gcf().set_size_inches(15, 10)
-plt.savefig(f"{FIGURES_DIR}/{title}")
-plt.close()
+# plt.savefig(f"{FIGURES_DIR}/{title}")
+# plt.close()
+plt.show()
+
+presence_unseen = load_scores_tf(DECOD_TFR, 'decoding_time-frequency_presence-unseen', average = False) 
+presence_unseen_avg = np.mean(presence_unseen, axis = 0)
+
+# # Wilcoxon signed-rank test with FDR correction
+x = presence_unseen
+reject, p_corrected, sign_points = wilcoxon_fdr_tfdecod(x)
+
+# plot results with contour for significant time-frequency bins
+title = "Decoding Gabor tilt on time-frequency data"
+av_tfr = AverageTFR(create_info(['freq'], sfreq), presence_unseen_avg[np.newaxis, :], freqs = freqs[0:], nave=0, times=times)
+av_tfr.plot('all', title=title, cmap='Blues', vmin = 0.5, vmax = np.abs(presence_unseen_avg).max(), 
+            mask = tilt_sign_points, mask_style = 'contour', yscale='linear'
+            )
+plt.gcf().set_size_inches(15, 10)
+# plt.savefig(f"{FIGURES_DIR}/{title}")
+# plt.close()
+plt.show()
